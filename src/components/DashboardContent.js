b4a027484c7d600e8ad65564eb5d2097a3276204
Faisal Karim <faisalchy64@gmail.com>
2022-05-27 07:24:36 +0600

    show orders on myorders page

--- src/components/DashboardContent.js
+++ src/components/DashboardContent.js
@@ -4,9 +4,7 @@ function DashboardContent({ children }) {
     return (
         <div className="drawer drawer-mobile">
             <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
-            <div className="drawer-content flex flex-col items-center justify-center">
-                {children}
-            </div>
+            <div className="drawer-content m-10">{children}</div>
             <div className="drawer-side">
                 <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
                 <ul className="menu p-4 overflow-y-auto w-60 bg-base-200 text-base-content">
--- src/pages/MyOrders.js
+++ src/pages/MyOrders.js
@@ -1,5 +1,100 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import auth from "../firebase";
+
 function MyOrders() {
-    return <h1>My order page.</h1>;
+    const [orders, setOrders] = useState([]);
+    const [id, setId] = useState("");
+    const [user] = useAuthState(auth);
+
+    useEffect(() => {
+        (async () => {
+            const res = await axios.get(
+                `http://localhost:5000/orders?email=${user.email}`
+            );
+
+            setOrders(res.data);
+        })();
+    }, [orders, user.email]);
+
+    // delete a specific order item from database
+
+    const handleConfirm = async (confirm) => {
+        if (confirm) {
+            const res = await axios.delete(`http://localhost:5000/order/${id}`);
+
+            console.log(res.data);
+        }
+    };
+
+    return (
+        <section>
+            <h1 className="mb-5 text-3xl md:text-5xl font-bold text-center">
+                My Orders
+            </h1>
+            <div className="overflow-x-auto">
+                <table className="table table-zebra w-full">
+                    <thead>
+                        <tr>
+                            <th>Product</th>
+                            <th>Transaction</th>
+                            <th>Price($)</th>
+                            <th>Quantity</th>
+                            <th>Pay</th>
+                            <th>Edit</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {orders.map((order) => (
+                            <tr key={order._id}>
+                                <td>{order.productName}</td>
+                                <td>Null</td>
+                                <td>{order.price}</td>
+                                <td>{order.quantity}</td>
+                                <td>
+                                    <button className="btn btn-primary">
+                                        Pay Now
+                                    </button>
+                                </td>
+                                <td>
+                                    <label
+                                        htmlFor="my-modal-6"
+                                        className="btn btn-error modal-button"
+                                        onClick={() => setId(order._id)}
+                                    >
+                                        Cancel
+                                    </label>
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
+
+            <input type="checkbox" id="my-modal-6" className="modal-toggle" />
+            <div className="modal modal-bottom sm:modal-middle">
+                <div className="modal-box">
+                    <h3 className="font-bold text-lg">
+                        Are You Sure, You Want To Cancel The Order!
+                    </h3>
+                    <div className="modal-action">
+                        <label
+                            onClick={() => handleConfirm("confirm")}
+                            htmlFor="my-modal-6"
+                            className="btn btn-success"
+                        >
+                            Confirm
+                        </label>
+
+                        <label htmlFor="my-modal-6" className="btn btn-error">
+                            Cancel
+                        </label>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
 }
 
 export default MyOrders;
