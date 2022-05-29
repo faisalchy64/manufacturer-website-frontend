8a034f46d69927bb63ca93dc7091f48db64092ef
Faisal Karim <faisalchy64@gmail.com>
2022-05-29 21:15:41 +0600

    update navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,31 +11,33 @@ function Navbar() {
 
     return (
         <div className="navbar justify-between bg-base-100">
-            {pathname.includes("/dashboard") && (
-                <label
-                    htmlFor="my-drawer-2"
-                    className="btn btn-square btn-ghost lg:hidden"
-                >
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        className="inline-block w-5 h-5 stroke-current"
+            <div>
+                {pathname.includes("/dashboard") && (
+                    <label
+                        htmlFor="my-drawer-2"
+                        className="btn btn-square btn-ghost lg:hidden"
                     >
-                        <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            strokeWidth="2"
-                            d="M4 6h16M4 12h16M4 18h16"
-                        ></path>
-                    </svg>
-                </label>
-            )}
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            className="inline-block w-5 h-5 stroke-current"
+                        >
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth="2"
+                                d="M4 6h16M4 12h16M4 18h16"
+                            ></path>
+                        </svg>
+                    </label>
+                )}
 
-            <div className="">
-                <Link to="/" className="btn btn-ghost normal-case text-xl">
-                    Comparts
-                </Link>
+                <div className="">
+                    <Link to="/" className="btn btn-ghost normal-case text-xl">
+                        Comparts
+                    </Link>
+                </div>
             </div>
 
             <div className="navbar-end grow hidden lg:flex">
